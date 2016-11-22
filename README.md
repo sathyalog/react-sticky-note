afc9a60f9c3394f0ade05429d7ee7472fd811a8d
svaka1 <svakacharla@sapient.com>
2016-11-22 20:04:35 +0530

    created basic sticky note using react and handled events

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # react-sticky-note
 A simple sticky note app developed using reactjs.
+
+**How to Run?**
+Just run **gulp**.
--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,33 @@
 'use strict';
 
 var gulp = require('gulp');
+var sourcemaps = require("gulp-sourcemaps");
 var webserver = require('gulp-webserver');
+var babel = require("gulp-babel");
+var concat = require("gulp-concat");
 var dest = 'prod/';
 
 gulp.task('default', ['webserver']);
 
 gulp.task('webserver', function() {
     gulp.src(dest)
+    .pipe(babel())
     .pipe(webserver({
         livereload: true,
         open: true,
     }));
 });
+
+
+// gulp.task("webserver", function () {
+//   return gulp.src("prod/**/*.js")
+//     .pipe(sourcemaps.init())
+//     .pipe(babel())
+//     .pipe(concat("all.js"))
+//     .pipe(sourcemaps.write("."))
+//     .pipe(gulp.dest("dest"))
+//     .pipe(webserver({
+//         livereload: true,
+//         open: true,
+//     }));
+// });
